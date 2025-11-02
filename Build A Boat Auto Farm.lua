6fa3370a5b362ffa33bb4a7fefc946fcfdaaa037
JABA <jabahsigma@gmail.com>
2025-11-02 17:18:48 +0400

    Auto farm.lua

--- Build A Boat Auto Farm.lua
+++ Build A Boat Auto Farm.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/jabahsigma-collab/JABAHUB/refs/heads/main/Loader.lua"))()
-task.wait(3)
+task.wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/jabahsigma-collab/Build-A-Boat-Script/refs/heads/main/Auto%20Farm.lua"))()
